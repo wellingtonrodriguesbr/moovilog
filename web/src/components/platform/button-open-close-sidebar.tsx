874149b746a7fdb9d15f516acb0a23fc426d24b4
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-06-18 16:34:43 -0300

    chore: update button icons

--- web/src/components/platform/button-open-close-sidebar.tsx
+++ web/src/components/platform/button-open-close-sidebar.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Button } from "../ui/button";
 import { useOpenCloseSidebar } from "@/providers/sidebar-provider";
 
 export function ButtonOpenCloseSidebar() {
-  const { handleOpenAndCloseSidebar } = useOpenCloseSidebar();
+  const { handleOpenAndCloseSidebar, isOpen } = useOpenCloseSidebar();
 
   return (
     <Button
       onClick={handleOpenAndCloseSidebar}
-      className="bg-zinc-100 hover:bg-zinc-200 text-app-blue-900"
+      className="bg-zinc-100 hover:bg-zinc-100 md:hover:bg-zinc-200 text-app-blue-900"
     >
-      <Menu className="size-4" />
+      {isOpen ? (
+        <>
+          <X className="block xl:hidden size-4" />
+          <Menu className="hidden xl:block size-4" />
+        </>
+      ) : (
+        <Menu className="size-4" />
+      )}
     </Button>
   );
 }
