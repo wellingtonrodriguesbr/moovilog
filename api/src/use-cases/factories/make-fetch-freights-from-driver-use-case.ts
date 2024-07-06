8d521ca340c998f6156c803a40f9739aa362b3e9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-06 19:42:38 -0300

    chore: replace import

--- api/src/use-cases/factories/make-fetch-freights-from-driver-use-case.ts
+++ api/src/use-cases/factories/make-fetch-freights-from-driver-use-case.ts
@@ -1,5 +1,5 @@
 import { PrismaDriversRepository } from "@/repositories/prisma/prisma-drivers-repository";
-import { FetchFreightsFromDriverUseCase } from "../fetch-freights-from-driver";
+import { FetchFreightsFromDriverUseCase } from "../fetch-freights-from-driver-use-case";
 import { PrismaFreightsRepository } from "@/repositories/prisma/prisma-freights-repository";
 
 export function makeFetchFreightsFromDriverUseCase() {
