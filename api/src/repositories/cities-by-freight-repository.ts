f6f8c1a652f145a47d986968668bc78557577407
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-13 13:07:57 -0300

    feat: create cities by freight repositories

---
+++ api/src/repositories/cities-by-freight-repository.ts
@@ -0,0 +1,8 @@
+export interface CreateManyParams {
+  freightId: string;
+  citiesIds: string[];
+}
+
+export interface CitiesByFreightRepository {
+  createMany(params: CreateManyParams): Promise<void>;
+}
