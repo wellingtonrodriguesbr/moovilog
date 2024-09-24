64273b479b78f387eab8c6be8f3de2aa47ff1230
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-09-24 19:37:40 -0300

    style: update button variant colors

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
 					"bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
 				outline:
 					"border border-zinc-200 bg-white hover:bg-app-cyan-100 hover:text-app-blue-900 hover:border-transparent bg-transparent",
-				secondary: "bg-zinc-100/50",
+				secondary: "bg-zinc-100 hover:bg-zinc-200/50",
 				ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
 				link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
 			},
