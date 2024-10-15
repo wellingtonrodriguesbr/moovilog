5d4b0f0c16e860142dff4cd7cae0c4b86ef0499a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-15 09:18:55 -0300

    chore: update db schema

---
+++ api/prisma/migrations/20241015121830_create_tables/migration.sql
@@ -0,0 +1,315 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MANAGER', 'FINANCIAL', 'COMERCIAL', 'OPERATIONAL');
+
+-- CreateEnum
+CREATE TYPE "CompanySize" AS ENUM ('MICRO', 'SMALL', 'MEDIUM', 'BIG');
+
+-- CreateEnum
+CREATE TYPE "VehicleCategory" AS ENUM ('UTILITY', 'VAN', 'LIGHT_TRUCKS', 'STRAIGHT_TRUCKS', 'TRUCKS', 'QUAD_AXLE_TRUCKS', 'SEMI_TRAILER', 'TANDEM_AXLE_TRUCK');
+
+-- CreateEnum
+CREATE TYPE "VehicleBody" AS ENUM ('CLOSED', 'OPEN', 'SIDER', 'REFRIGERATED', 'BUCKET');
+
+-- CreateEnum
+CREATE TYPE "VehicleType" AS ENUM ('OWN', 'OUTSOURCED', 'RENTED');
+
+-- CreateEnum
+CREATE TYPE "FreightType" AS ENUM ('FRACTIONAL', 'DIRECT', 'DEDICATED');
+
+-- CreateEnum
+CREATE TYPE "FreightModality" AS ENUM ('DAILY', 'PERCENTAGE', 'PRODUCTIVITY');
+
+-- CreateEnum
+CREATE TYPE "AccountTypeOfBankDetails" AS ENUM ('CURRENT_ACCOUNT', 'SALARY_ACCOUNT', 'SAVINGS_ACCOUNT');
+
+-- CreateEnum
+CREATE TYPE "AccountStatus" AS ENUM ('PENDING', 'ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "DriverType" AS ENUM ('INTERNAL', 'FREELANCER', 'AGGREGATE');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document_number" TEXT NOT NULL,
+    "size" "CompanySize" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "address_id" TEXT NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "company_members" (
+    "id" TEXT NOT NULL,
+    "sector" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "status" "AccountStatus" NOT NULL DEFAULT 'PENDING',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "company_id" TEXT NOT NULL,
+
+    CONSTRAINT "company_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "drivers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document_number" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "status" "AccountStatus" NOT NULL DEFAULT 'ACTIVE',
+    "type" "DriverType" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "creator_id" TEXT NOT NULL,
+    "company_id" TEXT NOT NULL,
+    "address_id" TEXT NOT NULL,
+
+    CONSTRAINT "drivers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bank_details" (
+    "id" TEXT NOT NULL,
+    "financial_institution" TEXT NOT NULL,
+    "account_type" "AccountTypeOfBankDetails" NOT NULL,
+    "agency" INTEGER NOT NULL,
+    "account_number" VARCHAR(20) NOT NULL,
+    "pix_key" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "bank_details_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "vehicles" (
+    "id" TEXT NOT NULL,
+    "plate" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "brand" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "category" "VehicleCategory" NOT NULL,
+    "type" "VehicleType" NOT NULL,
+    "body" "VehicleBody" NOT NULL,
+    "full_load_capacity" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "vehicles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "freights" (
+    "id" TEXT NOT NULL,
+    "type" "FreightType" NOT NULL,
+    "modality" "FreightModality" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "pickups_quantity" INTEGER,
+    "deliveries_quantity" INTEGER NOT NULL,
+    "total_weight_of_pickups" DECIMAL(10,2),
+    "total_weight_of_deliveries" DECIMAL(10,2) NOT NULL,
+    "freight_amount_in_cents" INTEGER NOT NULL,
+    "observation" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "driver_id" TEXT NOT NULL,
+    "vehicle_id" TEXT NOT NULL,
+    "creator_id" TEXT NOT NULL,
+    "company_id" TEXT NOT NULL,
+
+    CONSTRAINT "freights_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "routes" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "company_id" TEXT NOT NULL,
+
+    CONSTRAINT "routes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cities_in_route" (
+    "id" TEXT NOT NULL,
+    "route_id" TEXT NOT NULL,
+    "city_id" TEXT NOT NULL,
+
+    CONSTRAINT "cities_in_route_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "states" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "acronym" TEXT NOT NULL,
+
+    CONSTRAINT "states_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "areas" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+
+    CONSTRAINT "areas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "areas_in_state" (
+    "id" TEXT NOT NULL,
+    "state_id" TEXT NOT NULL,
+    "area_id" TEXT NOT NULL,
+
+    CONSTRAINT "areas_in_state_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cities" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "state_id" TEXT NOT NULL,
+
+    CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cities_in_area" (
+    "id" TEXT NOT NULL,
+    "city_id" TEXT NOT NULL,
+    "area_id" TEXT NOT NULL,
+
+    CONSTRAINT "cities_in_area_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" TEXT NOT NULL,
+    "zip_code" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "complement" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "city_id" TEXT NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_document_number_key" ON "companies"("document_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_owner_id_key" ON "companies"("owner_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_members_company_id_user_id_key" ON "company_members"("company_id", "user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drivers_document_number_key" ON "drivers"("document_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drivers_phone_key" ON "drivers"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "vehicles_plate_key" ON "vehicles"("plate");
+
+-- CreateIndex
+CREATE INDEX "freights_date_idx" ON "freights"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "states_acronym_key" ON "states"("acronym");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "areas_code_key" ON "areas"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cities_state_id_name_key" ON "cities"("state_id", "name");
+
+-- AddForeignKey
+ALTER TABLE "companies" ADD CONSTRAINT "companies_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies" ADD CONSTRAINT "companies_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "addresses"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company_members" ADD CONSTRAINT "company_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "company_members" ADD CONSTRAINT "company_members_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "drivers" ADD CONSTRAINT "drivers_creator_id_fkey" FOREIGN KEY ("creator_id") REFERENCES "company_members"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "drivers" ADD CONSTRAINT "drivers_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "drivers" ADD CONSTRAINT "drivers_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "addresses"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "freights" ADD CONSTRAINT "freights_driver_id_fkey" FOREIGN KEY ("driver_id") REFERENCES "drivers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "freights" ADD CONSTRAINT "freights_vehicle_id_fkey" FOREIGN KEY ("vehicle_id") REFERENCES "vehicles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "freights" ADD CONSTRAINT "freights_creator_id_fkey" FOREIGN KEY ("creator_id") REFERENCES "company_members"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "freights" ADD CONSTRAINT "freights_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "routes" ADD CONSTRAINT "routes_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cities_in_route" ADD CONSTRAINT "cities_in_route_route_id_fkey" FOREIGN KEY ("route_id") REFERENCES "routes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cities_in_route" ADD CONSTRAINT "cities_in_route_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "areas_in_state" ADD CONSTRAINT "areas_in_state_state_id_fkey" FOREIGN KEY ("state_id") REFERENCES "states"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "areas_in_state" ADD CONSTRAINT "areas_in_state_area_id_fkey" FOREIGN KEY ("area_id") REFERENCES "areas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cities" ADD CONSTRAINT "cities_state_id_fkey" FOREIGN KEY ("state_id") REFERENCES "states"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cities_in_area" ADD CONSTRAINT "cities_in_area_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cities_in_area" ADD CONSTRAINT "cities_in_area_area_id_fkey" FOREIGN KEY ("area_id") REFERENCES "areas"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- api/src/repositories/freight-information-repository.ts
+++
@@ -1,8 +0,0 @@
-import { FreightInformation, Prisma } from "@prisma/client";
-
-export interface FreightInformationRepository {
-	create(
-		data: Prisma.FreightInformationUncheckedCreateInput
-	): Promise<FreightInformation>;
-	findByFreight(freightId: string): Promise<FreightInformation | null>;
-}
