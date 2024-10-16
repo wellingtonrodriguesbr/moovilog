75278d0f0214fccd310b0ed81490ed136fd91f41
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-16 07:26:11 -0300

    refactor: refactor logout controller

--- api/src/http/controllers/logout-controller.ts
+++ api/src/http/controllers/logout-controller.ts
@@ -1,33 +1,23 @@
 import { UnauthorizedError } from "@/use-cases/errors/unauthorized-error";
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export async function logoutController(
-	req: FastifyRequest,
-	reply: FastifyReply
-) {
-	await req.jwtVerify({ onlyCookie: true });
+export class LogoutController {
+	static async handle(req: FastifyRequest, reply: FastifyReply) {
+		try {
+			await req.jwtVerify({ onlyCookie: true });
 
-	try {
-		reply.clearCookie("refreshToken", {
-			path: "/",
-			domain: process.env.COOKIES_DOMAIN,
-		});
-		await reply.jwtSign(
-			{},
-			{
-				sign: {
-					sub: req.user.sub,
-					expiresIn: 1,
-				},
+			reply.clearCookie("refreshToken", {
+				path: "/",
+				domain: process.env.COOKIES_DOMAIN,
+			});
+
+			return reply.send({ success: true });
+		} catch (error) {
+			if (error instanceof UnauthorizedError) {
+				return reply.status(401).send({ message: error.message });
+			} else {
+				return reply.status(500).send({ message: "Internal server error" });
 			}
-		);
-
-		return reply.send({ success: true });
-	} catch (error) {
-		if (error instanceof UnauthorizedError) {
-			return reply.status(401).send({ message: error.message });
-		} else {
-			return reply.status(500).send({ message: "Internal server error" });
 		}
 	}
 }
--- api/src/http/routes/users.ts
+++ api/src/http/routes/users.ts
@@ -1,16 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { getUserProfileController } from "../controllers/get-user-profile-controller";
-import { verifyJWT } from "../middlewares/verify-jwt";
-import { logoutController } from "../controllers/logout-controller";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { RegisterUserController } from "@/http/controllers/register-user-controller";
 import { AuthenticateController } from "@/http/controllers/authenticate-controller";
 import { RefreshToken } from "@/http/controllers/refresh-token";
+import { LogoutController } from "@/http/controllers/logout-controller";
 
 export async function usersRoutes(app: FastifyInstance) {
 	app.post("/users", RegisterUserController.handle);
 	app.post("/sessions", AuthenticateController.handle);
 	app.patch("/token/refresh", RefreshToken.handle);
-	app.post("/logout", logoutController);
+	app.post("/logout", LogoutController.handle);
 
 	app.get("/me", { onRequest: [verifyJWT] }, getUserProfileController);
 }
