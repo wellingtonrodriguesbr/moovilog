d6b7e9e54e7c4a135ac52a3ccbe4b2eb3386c11a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-16 07:19:53 -0300

    refactor: refactor refresh token controller

--- api/src/http/routes/users.ts
+++ api/src/http/routes/users.ts
@@ -1,15 +1,15 @@
 import { FastifyInstance } from "fastify";
-import { refreshController } from "../controllers/refresh-token";
 import { getUserProfileController } from "../controllers/get-user-profile-controller";
 import { verifyJWT } from "../middlewares/verify-jwt";
 import { logoutController } from "../controllers/logout-controller";
 import { RegisterUserController } from "@/http/controllers/register-user-controller";
 import { AuthenticateController } from "@/http/controllers/authenticate-controller";
+import { RefreshToken } from "@/http/controllers/refresh-token";
 
 export async function usersRoutes(app: FastifyInstance) {
 	app.post("/users", RegisterUserController.handle);
 	app.post("/sessions", AuthenticateController.handle);
-	app.patch("/token/refresh", refreshController);
+	app.patch("/token/refresh", RefreshToken.handle);
 	app.post("/logout", logoutController);
 
 	app.get("/me", { onRequest: [verifyJWT] }, getUserProfileController);
