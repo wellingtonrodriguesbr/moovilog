9137c88bdc09c7f13344694a0276f47956bcc0e4
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-10-18 07:06:53 -0300

    refactor(companies): refactor create company controller

--- api/src/http/controllers/register-company-controller.ts
+++ api/src/http/controllers/register-company-controller.ts
@@ -1,50 +1,43 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CompanyAlreadyExistsError } from "@/use-cases/errors/company-already-exists-error";
+import { UserAlreadyHasACompanyError } from "@/use-cases/errors/user-already-has-a-company-error";
 import { makeRegisterCompanyUseCase } from "@/use-cases/factories/make-register-company-use-case";
-import { Prisma } from "@prisma/client";
 
 import z from "zod";
 
-export async function registerCompanyController(
-	req: FastifyRequest,
-	reply: FastifyReply
-) {
-	const registerCompanyBodySchema = z.object({
-		name: z.string(),
-		documentNumber: z.string().min(14).max(14),
-		type: z.enum(["HEADQUARTERS", "BRANCH", "AGENCY"]),
-		size: z.enum(["MICRO", "SMALL", "MEDIUM", "BIG"]),
-	});
-
-	const { name, documentNumber, type, size } = registerCompanyBodySchema.parse(
-		req.body
-	);
-	const ownerId = req.user.sub;
-
-	try {
-		const registerCompanyUseCase = makeRegisterCompanyUseCase();
-		const { company } = await registerCompanyUseCase.execute({
-			ownerId,
-			name,
-			documentNumber,
-			type,
-			size,
+export class RegisterCompanyController {
+	static async handle(req: FastifyRequest, reply: FastifyReply) {
+		const registerCompanyBodySchema = z.object({
+			name: z.string(),
+			documentNumber: z.string().min(14).max(14),
+			size: z.enum(["MICRO", "SMALL", "MEDIUM", "BIG"]),
+			ownerSector: z.string(),
 		});
 
-		reply.status(201).send({ company });
-	} catch (error) {
-		if (error instanceof Prisma.PrismaClientKnownRequestError) {
-			if (error.code === "P2002") {
-				reply.status(409).send({
-					message:
-						"User already owns a company. Conflict with existing resource.",
-				});
-			}
-		}
-		if (error instanceof CompanyAlreadyExistsError) {
-			reply.status(409).send({ message: error.message });
-		}
+		const { name, documentNumber, size, ownerSector } =
+			registerCompanyBodySchema.parse(req.body);
+		const ownerId = req.user.sub;
 
-		throw error;
+		try {
+			const registerCompanyUseCase = makeRegisterCompanyUseCase();
+			const { company } = await registerCompanyUseCase.execute({
+				ownerId,
+				name,
+				documentNumber,
+				size,
+				ownerSector,
+			});
+
+			reply.status(201).send({ company });
+		} catch (error) {
+			if (error instanceof UserAlreadyHasACompanyError) {
+				reply.status(409).send({ message: error.message });
+			}
+			if (error instanceof CompanyAlreadyExistsError) {
+				reply.status(409).send({ message: error.message });
+			}
+
+			throw error;
+		}
 	}
 }
