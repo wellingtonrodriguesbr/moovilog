ea82f83bde59c4389fa5f587303c640e4447592f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-11-20 09:14:08 -0300

    chore: remove unused repository

--- api/src/use-cases/factories/make-register-company-address-use-case.ts
+++ api/src/use-cases/factories/make-register-company-address-use-case.ts
@@ -2,16 +2,13 @@ import { PrismaCompaniesRepository } from "@/repositories/prisma/prisma-companie
 import { RegisterCompanyAddressUseCase } from "../register-company-address-use-case";
 import { PrismaCitiesRepository } from "@/repositories/prisma/prisma-cities-repository";
 import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
-import { PrismaCompanyAddressesRepository } from "@/repositories/prisma/prisma-company-addresses-repository";
 
 export function makeRegisterCompanyAddressUseCase() {
 	const citiesRepository = new PrismaCitiesRepository();
 	const companiesRepository = new PrismaCompaniesRepository();
-	const companyAddressRepository = new PrismaCompanyAddressesRepository();
 	const addressesRepository = new PrismaAddressesRepository();
 
 	const registerCompanyAddressUseCase = new RegisterCompanyAddressUseCase(
-		companyAddressRepository,
 		addressesRepository,
 		citiesRepository,
 		companiesRepository
