b51a39933a85f2c5f87ac3874bc75a31ecf69e1f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-11-20 08:40:14 -0300

    test: refactor fetch freights test

--- api/src/use-cases/fetch-freights-from-company-use-case.spec.ts
+++ api/src/use-cases/fetch-freights-from-company-use-case.spec.ts
@@ -35,27 +35,31 @@ describe("Fetch freights from company use case", () => {
 			name: "Company name",
 			documentNumber: "12312312389899",
 			size: "MEDIUM",
-			type: "HEADQUARTERS",
-			ownerId: "john-doe-id-01",
+			ownerId: "john-doe-01",
 		});
 
 		await companyMembersRepository.create({
+			id: "company-member-id-01",
 			companyId: "company-id-01",
-			memberId: "john-doe-id-01",
+			userId: "john-doe-id-01",
+			sector: "Diretoria",
 			role: "ADMIN",
 		});
 
 		await freightsRepository.create({
-			date: new Date("2024-07-18 09:00:00"),
+			date: new Date(),
 			type: "FRACTIONAL",
 			deliveriesQuantity: 12,
 			pickupsQuantity: 5,
 			totalWeightOfDeliveries: 2400,
 			totalWeightOfPickups: 1000,
-			freightAmountInCents: 60000,
-			creatorId: "john-doe-id-01",
-			driverId: "john-doe-driver-id",
+			freightAmountInCents: 600,
+			modality: "DAILY",
+			creatorId: "company-member-id-01",
 			companyId: "company-id-01",
+			driverId: "fake-driver-id-01",
+			routeId: "fake-route-01",
+			vehicleId: "fake-vehicle-id-01",
 		});
 	});
 
