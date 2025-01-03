eee18b2b178303a0a4d52b924285495ee6753b5f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-01-03 11:46:49 -0300

    chore: replace auth pages

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
-import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+
+import { ArrowLeft } from "lucide-react";
 
 export default function AuthLayout({
 	children,
@@ -8,9 +9,8 @@ export default function AuthLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<main className="w-full h-screen grid grid-cols-1 md:grid-cols-[1fr_450px]">
-			<div className="hidden md:block w-full h-full bg-zinc-100"></div>
-			<aside className="w-full h-full flex flex-col justify-center p-4 md:p-8">
+		<main className="w-full h-screen flex items-center justify-center">
+			<aside className="w-[600px] flex flex-col p-4 md:p-8">
 				<header className="flex flex-col gap-8 mb-6">
 					<Link
 						href="/"
