f1402f34a190f4abddd3434769531ce11e35f748
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-01-23 07:28:07 -0300

    refactor(wip): refactor use cases

--- api/src/http/routes/companies.ts
+++ api/src/http/routes/companies.ts
@@ -1,19 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../middlewares/verify-jwt";
-import { GetCompanyInformationController } from "@/http/controllers/get-company-information-controller";
-import { RegisterCompanyAreaServiceController } from "@/http/controllers/register-company-area-service-controller";
 import { FetchAreasStatesFromCompanyController } from "@/http/controllers/fetch-areas-states-from-company-controller";
 
 export async function companiesRoutes(app: FastifyInstance) {
 	app.addHook("onRequest", verifyJWT);
 
-	app.post(
-		"/companies/:companyId/areas-states",
-		RegisterCompanyAreaServiceController.handle
-	);
 	app.get(
 		"/companies/:companyId/areas-states",
 		FetchAreasStatesFromCompanyController.handle
 	);
-	app.get("/companies/information", GetCompanyInformationController.handle);
 }
