62dd7c6c01a474728f0e40bb6ea823fab3eec4e9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-02-25 09:01:22 -0300

    update

--- web/src/components/platform/pages/home/index.tsx
+++ web/src/components/platform/pages/home/index.tsx
@@ -5,12 +5,13 @@ import { useGetProfile } from "@/hooks/user/use-get-profile";
 import { QuickAccess } from "@/components/platform/pages/home/components/quick-access";
 import { BarChartComponent } from "@/components/platform/pages/home/components/home-metrics/bar-chart";
 import { PieChartComponent } from "@/components/platform/pages/home/components/home-metrics/pie-chart";
+import { Construction } from "lucide-react";
 
 export function Home() {
 	const { profile, isGetProfilePending } = useGetProfile();
 
 	return (
-		<section className="mt-4 space-y-6 w-full">
+		<section className="flex flex-col mt-4 gap-8 w-full h-[85vh]">
 			<header className="flex justify-between">
 				<h1 className="text-2xl md:text-3xl font-medium">
 					{isGetProfilePending ? (
@@ -20,11 +21,18 @@ export function Home() {
 					)}
 				</h1>
 			</header>
-			<QuickAccess />
+			<div className="w-full h-full flex flex-col gap-4 items-center justify-center flex-1 bg-zinc-100 p-6 rounded-md">
+				<Construction className="size-12 md:size-24 text-amber-500" />
+				<h2 className="text-2xl md:text-4xl text-center max-w-2xl leading-normal md:leading-relaxed">
+					A página home está sendo construída, mas fique a vontade
+					para explorar! 🚀
+				</h2>
+			</div>
+			{/* <QuickAccess />
 			<div className="grid grid-cols-1 xl:grid-cols-[1fr_400px] gap-4">
 				<BarChartComponent />
 				<PieChartComponent />
-			</div>
+			</div> */}
 		</section>
 	);
 }
